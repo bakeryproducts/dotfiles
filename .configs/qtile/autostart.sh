724c2cd78388bb7847fa241f78d046b375955f5a
bakeryproducts <gleb.m.sokolov@gmail.com>
2020-10-26 19:49:20 +0300

    autostart.sh

--- .configs/qtile/autostart.sh
+++ .configs/qtile/autostart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 picom & 
-nitrogen --restore & 
-setxkbmap -option caps:escape
+#nitrogen --restore &
+#feh --bg-fill ~/Downloads/everest_mountain_sky_tops_96976_1920x1080.jpg &
+setxkbmap -option caps:escape &
+xset r rate 220 50 &
+clipmenud &
