26842d5c07f86a322cd682b935403074a5289450
bakeryproducts <gleb.m.sokolov@gmail.com>
2020-11-15 06:46:15 -0500

    dell fixes

--- .bash_aliases
+++ .bash_aliases
@@ -4,8 +4,11 @@ alias ls='ls --color=auto'
 alias ll='ls -alFh'
 alias la='ls -A'
 alias l='ls -CF'
+alias sai='sudo apt install'
+alias sau='sudo apt update'
 alias si='ssh $DB7USER@$DB7HOST'
 alias sj='echo $DB7USER'
+alias restart='sudo shutdown -r now'
 
 #zz()
 #{
--- .configs/qtile/autostart.sh
+++ .configs/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 picom & 
-#nitrogen --restore &
-#feh --bg-fill ~/Downloads/everest_mountain_sky_tops_96976_1920x1080.jpg &
+nitrogen --set-zoom-fill ~/Downloads/wp.jpg
+xrdb ~/.Xresources
 setxkbmap -option caps:escape &
 xset r rate 220 50 &
 clipmenud &
