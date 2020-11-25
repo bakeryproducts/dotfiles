c5057d916cd984b7f4f1953e12d3edc2d64a3b02
sokolov <gleb.m.sokolov@gmail.com>
2020-11-25 14:17:58 +0300

    fzf and fasd fixes

--- .configs/zsh/.fzf.zsh
+++ .configs/zsh/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/gsm/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/gsm/.fzf/bin"
+if [[ ! "$PATH" == */home/$USER/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/$USER/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/gsm/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/$USER/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/gsm/.fzf/shell/key-bindings.zsh"
+source "/home/$USER/.fzf/shell/key-bindings.zsh"
