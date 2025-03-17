7493cc96031cb3cc4e6f6f1598041bba327968a5
bakeryproducts <gleb.m.sokolov@gmail.com>
2025-03-17 22:54:46 +0100

    hypr fixes

--- .configs/qtile/autostart.sh
+++ .configs/qtile/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-picom -f & 
-nitrogen --set-zoom-fill ~/Downloads/wp.jpg
+#picom -f & 
+#nitrogen --set-zoom-fill ~/Downloads/wp.jpg
 xrdb ~/.config/xs/.Xresources
 setxkbmap -option caps:escape 
 xset r rate 220 50 &
