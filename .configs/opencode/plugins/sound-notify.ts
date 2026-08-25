fc20bdd3447230e14182aae066b873ee7cd2872b
bakeryproducts <gleb.m.sokolov@gmail.com>
2026-08-25 22:14:44 +0100

    oc

--- .configs/opencode/plugins/sound-notify.ts
+++ .configs/opencode/plugins/sound-notify.ts
@@ -1,14 +1,15 @@
 import type { Plugin } from "@opencode-ai/plugin"
+import { spawn } from "node:child_process"
 
 const SOUNDS = "/home/gsm/Documents/gmcp/oc-bridge-plugin/data/sounds"
 const PERMISSION_SOUND = `${SOUNDS}/question/painsharp04.mp3`
 const IDLE_SOUND = `${SOUNDS}/message/gunslingerpunch03.mp3`
 
-export default (async ({ $ }) => {
-  function play(file: string, volume: number) {
-    $`paplay --volume=${volume} ${file}`.quiet().nothrow()
-  }
+function play(file: string, volume: number) {
+  spawn("paplay", [`--volume=${volume}`, file], { stdio: "ignore", detached: true }).unref()
+}
 
+export default (async () => {
   return {
     event: async ({ event }) => {
       if (event.type === "permission.asked") {
